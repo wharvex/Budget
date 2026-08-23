0be4179c92acdcc3c1e614c083e325a465e2265a
Timothy Gudlewski <t.gudlewski@gmail.com>
2026-08-23 12:19:23 -0400

    add expense_type and account create table scripts

---
+++ scripts/create_account_table.sql
@@ -0,0 +1,4 @@
+CREATE TABLE account (
+    account_id INTEGER PRIMARY KEY,
+    account_name VARCHAR(255) NOT NULL
+);
---
+++ scripts/create_expense_type_table.sql
@@ -0,0 +1,4 @@
+CREATE TABLE expense_type (
+    expense_type_id INTEGER PRIMARY KEY,
+    expense_type_name VARCHAR(255) NOT NULL
+);
