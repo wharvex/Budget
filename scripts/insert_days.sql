93064350898704773b1d4bd790819b79f84f9e6d
Timothy Gudlewski <t.gudlewski@gmail.com>
2026-08-23 12:06:32 -0400

    add day insert script

---
+++ scripts/insert_days.sql
@@ -0,0 +1,11 @@
+INSERT INTO day (day_id, month, day, is_last_of_month)
+SELECT
+    EXTRACT(DOY FROM calendar_date)::INTEGER,
+    EXTRACT(MONTH FROM calendar_date)::INTEGER,
+    EXTRACT(DAY FROM calendar_date)::INTEGER,
+    calendar_date = (DATE_TRUNC('month', calendar_date) + INTERVAL '1 month - 1 day')::DATE
+FROM GENERATE_SERIES(
+    DATE '2001-01-01',
+    DATE '2001-12-31',
+    INTERVAL '1 day'
+) AS calendar(calendar_date);
