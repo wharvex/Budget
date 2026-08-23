c4be755a53565f2183364ab35353e07d088e13da
Timothy Gudlewski <t.gudlewski@gmail.com>
2026-08-23 14:34:13 -0400

    add insert expense types script

---
+++ scripts/dml_07_insert_expense_types.sql
@@ -0,0 +1,14 @@
+INSERT INTO expense_type (expense_type_name)
+VALUES
+    ('Housing'),
+    ('Utilities'),
+    ('Groceries'),
+    ('Dining'),
+    ('Transportation'),
+    ('Healthcare'),
+    ('Insurance'),
+    ('Entertainment'),
+    ('Personal Care'),
+    ('Education'),
+    ('Savings'),
+    ('Miscellaneous');
